580aa7d05122afa624ce4ea21e695703029cc615
Mike <michaelokolo62@yahoo.com>
2025-03-03 22:48:13 +0000

    setting up project

---
+++ app/actions/auth.ts
@@ -0,0 +1,16 @@
+import { SignupFormSchema, FormState } from '@/app/lib/definitions';
+
+export async function signup(state: FormState, formData: FormData) {
+  const validatedFields = SignupFormSchema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  });
+
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+}
