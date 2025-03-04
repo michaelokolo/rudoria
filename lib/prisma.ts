97fa7b5f25346b29730d5440586c79759ba2e5ed
Mike <michaelokolo62@yahoo.com>
2025-03-04 02:22:09 +0000

    fixing bugs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
