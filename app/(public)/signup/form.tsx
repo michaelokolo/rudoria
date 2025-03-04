b86e87d380ca67329c2bef209de43ef3a899ae5d
Mike <michaelokolo62@yahoo.com>
2025-03-04 02:47:17 +0000

    trigger build

--- app/(public)/signup/form.tsx
+++ app/(public)/signup/form.tsx
@@ -7,6 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 
+
 export default function SignupForm() {
   const [state, action] = useActionState(signup, undefined);
   return (
