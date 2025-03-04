046124bf41f316110f0b1418c8a84565b4386b58
Mike <michaelokolo62@yahoo.com>
2025-03-04 02:27:06 +0000

    Removed login link

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -45,7 +45,7 @@ export default function Layout({
           <div className="flex items-center space-x-4">
             <Link
               className="rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              href="/login"
+              href="#"
             >
               Login
             </Link>
