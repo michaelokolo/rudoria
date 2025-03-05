4e3c8885ae424efade233958f1057ff8ea046a5f
Mike <michaelokolo62@yahoo.com>
2025-03-05 21:22:06 +0000

    implemented updatesession

--- app/actions/database-session.ts
+++ app/actions/database-session.ts
@@ -63,3 +63,22 @@ export async function verifySession() {
 
   return { isAuth: true, userId: session.userId };
 }
+
+export async function updateSession() {
+  const session = (await cookies()).get('session')?.value;
+  const payload = await decrypt(session);
+
+  if (!session || !payload) {
+    return null;
+  }
+
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  (await cookies()).set('session', session, {
+    httpOnly: true,
+    secure: true,
+    expires: expires,
+    sameSite: 'lax',
+    path: '/',
+  });
+}
